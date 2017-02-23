cd84d78b7b8ff556cff5da14e566100d5d0183a8
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-02-23 15:39:05 +0100

    fixed problem on beg end tables

--- domain_analysis.py
+++ domain_analysis.py
@@ -31,17 +31,34 @@ def main():
     # analizing domains used
     #analyse_domains(Session) 
     
-    analyse_beg_end_day(Session)
+    table_info = analyse_beg_end_day(Session)
             
+    create_graph_beg_end_day(table_info)
+
+def create_graph_beg_end_day(table_info):
+
+    print ('entrou')
+    f, ax1 = plt.subplots(1, 1, figsize=(12, 8))
+    ax1.set_title('Beginning and End Times of Daily Usage [user=%s]'%('lalalalalal'))#%(uname))    
+    ax1.set_ylim((0,24))
+    ax1.set_ylabel('Hour of Day')
+    ax1.set_xlim(xstart,xend)
+    ax1.legend(loc='best')
+
+    plt.tight_layout()
+    plt.show()
+
+    #x = table_info[2]
+    #y = defaultdict(list)
 
 def analyse_beg_end_day(Session):
     ses = Session()
-
+    
     devices = ses.query(Device).distinct().order_by(Device.id)
 
     for dev in devices:
 
-        sql_end_day = text("SELECT devid, endts FROM flows join (SELECT DATE(endts) as date_entered, MAX(endts) as max_time FROM flows WHERE devid =:d_id GROUP BY date(endts)) AS grp ON grp.max_time = flows.endts order by flows.endts limit 400;").bindparams(d_id = dev.id)
+        sql_end_day = text("SELECT distinct devid, flows.endts FROM flows join (SELECT DATE(endts) as date_entered, MAX(endts) as max_time FROM flows WHERE devid =:d_id GROUP BY date(endts)) AS grp ON grp.max_time = flows.endts order by flows.endts;").bindparams(d_id = dev.id)
         result_end_day = ses.execute(sql_end_day)
 
         info = defaultdict(list)
@@ -49,16 +66,17 @@ def analyse_beg_end_day(Session):
             info['devid'].append(str(row[0]))
             info['end'].append(row[1])
 
-        sql_beg_day = text("SELECT devid, startts FROM flows join (SELECT DATE(startts) as date_entered, MIN(startts) as min_time FROM flows WHERE devid =:d_id GROUP BY date(startts)) AS grp ON grp.min_time = flows.startts order by flows.startts limit 400;").bindparams(d_id = dev.id)
+        sql_beg_day = text("SELECT distinct devid, flows.startts FROM flows join (SELECT DATE(startts) as date_entered, MIN(startts) as min_time FROM flows WHERE devid =:d_id GROUP BY date(startts)) AS grp ON grp.min_time = flows.startts order by flows.startts;").bindparams(d_id = dev.id)
         result_beg_day = ses.execute(sql_beg_day)
 
         for row in result_beg_day:
             info['start'].append(row[1])
 
-        table = pd.DataFrame(info)
-        display(table)
-
+        df = pd.DataFrame(info)
+        display(df)
+    print("888888888888888888")
     ses.close()
+    return info
 
 def analyse_domains (Session):
 
