a66b801feb4c234313586387c773f017328a753a
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-03-20 14:34:11 +0100

    making io scatter plots

--- io_analysis.py
+++ io_analysis.py
@@ -1,6 +1,6 @@
-#import numpy as np
+import numpy as np
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 
@@ -9,8 +9,6 @@ from collections import defaultdict
 from model_io.Base import Base
 from model_io.Devices import Devices
 from model_io.Io import Io
-#from model.User import User
-#from model.user_devices import user_devices
 
 from sqlalchemy import create_engine, text, func
 from sqlalchemy.orm import sessionmaker
@@ -65,7 +63,6 @@ def get_io_data():
                 info_end['ts_end'].append(row[1])
                 info_end['interaction'].append(row[2])
 
-
             info_beg = defaultdict(list)
             for row in result_beg_day:
                 info_beg['devid'].append(row[0])
@@ -89,15 +86,19 @@ def get_io_data():
                     info_beg['devid'].insert(cont, row[0])
                     info_beg['interaction'].insert(cont, row[2])
 
-            df_beg = pd.DataFrame(info_beg)
-            display(df_beg)
-            df_end = pd.DataFrame(info_end)
-            display(df_end)
+            #df_beg = pd.DataFrame(info_beg)
+            #display(df_beg)
+            #df_end = pd.DataFrame(info_end)
+            #display(df_end)
 
             days_str = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday', 'Sunday'}
 
             info_week_beg = analyze_per_day(info_beg, 'ts_start', device.device_id, days_str)
             info_week_end = analyze_per_day(info_end, 'ts_end', device.device_id, days_str)
+
+            #scatter plot
+            scatter_plot(info_week_beg, 'beginning', days_str)
+            scatter_plot(info_week_end, 'end', days_str)
 
 def analyze_per_day(info, key_beg_end, user, days_str):
 
@@ -109,19 +110,62 @@ def analyze_per_day(info, key_beg_end, user, days_str):
             day = timst
             weekday = day.strftime('%A')
             info_week[weekday].append(day)
-            #info_week['id'].append(user)
             info_week[weekday+'interaction'].append(info['interaction'][cont])
             cont = cont + 1
 
     info_week['user'] = user
-    for name in days_str:
-        df_col = defaultdict(list)
-        df_col[name + ' ' + key_beg_end] = info_week[name]
-        df_col['interaction'] = info_week[name+'interaction']
-        df_week = pd.DataFrame(df_col)
-        display(df_week)
+    #for name in days_str:
+        #df_col = defaultdict(list)
+        #df_col[name + ' ' + key_beg_end] = info_week[name]
+        #df_col['interaction'] = info_week[name+'interaction']
+        #df_week = pd.DataFrame(df_col)
+        #display(df_week)
 
     return info_week
+
+def scatter_plot(info_week, key_beg_end, days_str):
+    sns.set_style('darkgrid')
+    #for each user device make a scatter plot
+#    for dev in range (0, quantity_dev):
+    x = []
+    y = []
+    #print(info_week)
+    #if info_week['platform']:
+        #platform = info_week[dev]['platform'][0]
+    for weekday in days_str:
+        timst_list  = info_week[weekday]
+        for timst in timst_list:
+            wkday = convert_weekday(weekday)
+            x.append(wkday)
+            y.append(timst.hour+timst.minute/60.0)
+    _, num_x = np.unique(x, return_inverse=True)
+    plt.title('Io ' + key_beg_end + ' - user: ' + info_week['user'])
+    plt.ylabel('Hour of Day')
+    plt.ylim((0,24))
+    plt.xticks(num_x, x)
+    plt.scatter(num_x, y, s=20, c='b', alpha=0.5)
+    plt.savefig('figs_scatter_io/' + info_week['user'] + '-' + key_beg_end +  '-allweek.png')
+    plt.close()
+
+
+def convert_weekday(weekday):
+
+    if (weekday == 'Monday'):
+        return '0Mon'
+    elif (weekday == 'Tuesday'):
+        return '1Tue'
+    elif (weekday == 'Wednesday'):
+        return '2Wed'
+    elif (weekday == 'Thursday'):
+        return '3Thu'
+    elif (weekday == 'Friday'):
+        return '4Fri'
+    elif (weekday == 'Saturday'):
+        return '5Sat'
+    else:
+        return '6Sun'
+
+
 
 if __name__ == "__main__":
     get_io_data()
