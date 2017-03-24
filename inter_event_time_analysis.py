9d85fa3c306e86fc8266b7110beee20eda4c6d1a
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-03-24 17:06:05 +0100

    inter activity plot per timst with wrong spacing in y axis

--- inter_event_time_analysis.py
+++ inter_event_time_analysis.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-#%matplotlib inline 
+from matplotlib import dates
 
 from bokeh.plotting import figure, show, output_notebook
 from bokeh.charts import *
@@ -52,6 +52,7 @@ def main ():
             devs[d.id] = d.platform
 
         iat = []
+        traces = []
     
         sqlq = """SELECT ts,lag(ts) OVER (ORDER BY ts) FROM \
         (SELECT ts FROM dnsreqs WHERE devid = :d_id UNION ALL \
@@ -64,18 +65,53 @@ def main ():
                 if (row[1]==None):
                     continue
                 iat.append((row[0]-row[1]).total_seconds())
-                    #if (row[0]-row[1]).total_seconds() > 60*60*8:
+                traces.append(row[0])   
+                #if (row[0]-row[1]).total_seconds() > 60*60*8:
                     #print 'sleep'
                     #print row[1]
                     #print 'wake up'
                     #print row[0]
             
             if iat and user.username != 'desir':
-                plot_interval_times(iat, user, devs, elem_id)
+                #plot_cdf_interval_times(iat, user, devs, elem_id)
+                plot_traces(traces, user, devs, elem_id)
+
+
+def plot_traces(traces, user, devs, elem_id): 
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 4))
+    x = []
+    y = []
+    y_label = []
+    cont = 0
+    for timst in traces:
+        #while cont < 10:
+        if timst.date() not in y:
+            y_label.append(timst.date())
+        x.append(timst.hour+timst.minute/60.0+timst.second/3600.0)
+        y.append(timst.date())
+         #   cont = cont + 1
+
+    hfmt = dates.DateFormatter('%m-%d')
+    ax.yaxis.set_major_formatter(hfmt)
+
+    ax.plot(x,y, 'og', lw=2)
+    ax.set_title('Device usage [user=%s, device=%s]'%(user.username, devs[int(elem_id)]))
+    #ax1.set_ylabel('CDF')
+    print y_label
+    print len(y_label)
+    ax.set_yticks(y_label)
+    ax.set_ylim(y_label[0], y_label[len(y_label)-1])
+    #ax1.set_xscale('log')
+    #ax1.set_xlabel('seconds')
+    #ax1.set_xticks([0.001,1,60,3600,24*3600])
+    #ax1.set_xticklabels(['1ms','1s','1min','1h','1day'])
+    ax.set_xlim(0,24)
+    plt.show()
 
 
 
-def plot_interval_times(iat, user, devs, elem_id):
+def plot_cdf_interval_times(iat, user, devs, elem_id):
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
     (x,y) = datautils.aecdf(iat)
