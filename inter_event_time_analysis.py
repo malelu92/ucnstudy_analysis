709fd37792741636585c8b710048a75f47d9cf7b
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-03-24 15:49:27 +0100

    created function on inter traffic program to plot graphs

--- inter_event_time_analysis.py
+++ inter_event_time_analysis.py
@@ -16,8 +16,6 @@ from datetime import datetime, timedelta
 from model.Base import Base
 from model.User import User
 from model.Device import Device
-from model.DeviceTraffic import DeviceTraffic
-from model.DeviceAppTraffic import DeviceAppTraffic
 from model.HttpReq import HttpReq
 from model.DnsReq import DnsReq
 from model.user_devices import user_devices;
@@ -38,90 +36,82 @@ engine = create_engine(DB, echo=False, poolclass=NullPool)
 Base.metadata.bind = engine
 Session = sessionmaker(bind=engine) 
 
-
 ses = Session()
 users = ses.query(User)
 
-for user in users:
-    #u = ses.query(User).filter(User.username==uname).one()
-    print ('user : ' + user.username)
+def main ():
+    for user in users:
+        print ('user : ' + user.username)
 
-    #devids = ",".join([str(d.id) for d in user.devices if not d.shared])    
+        devids = []
+        for d in user.devices:
+            devids.append(str(d.id))
 
-    devids = []
-    for d in user.devices:
-        devids.append(str(d.id))
-    #devids = []
-    #devids = devids.append(str(d.id) for d in user.devices if not d.shared)
-    #print ('user: ' + user.username)
-    #print('devids: ' + str(devids))
+        devs = {}
+        for d in user.devices:
+            devs[d.id] = d.platform
 
-    devs = {}
-    for d in user.devices:
-        devs[d.id] = d.platform
-
-    #print ('devs :' + str(devs))
-
-    iat = []
+        iat = []
     
-    sqlq = """SELECT ts,lag(ts) OVER (ORDER BY ts) FROM \
-    (SELECT ts FROM dnsreqs WHERE devid = :d_id UNION ALL \
-     SELECT ts FROM httpreqs2 WHERE devid = :d_id AND user_url = 't' UNION ALL \
-    SELECT startts from flows WHERE devid = :d_id) \
-    AS events"""#%(devids,devids,devids)
-    #print 'lolo'
-    #print devids
-    print devs
-    for elem_id in devids:
-        print elem_id
-        #print devs[int(elem_id)]
-        for row in ses.execute(text(sqlq).bindparams(d_id = elem_id)):
-            #print 'lala'
-            if (row[1]==None):
-                continue
-            iat.append((row[0]-row[1]).total_seconds())
-
-        if iat and user.username != 'desir':
-            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
-            (x,y) = datautils.aecdf(iat)
-            ax1.plot(x,y, '-b', lw=2)
-            ax1.set_title('Inter-Event Time [user=%s, events=%d]'%(user.username, len(x)))    
-            ax1.set_ylabel('CDF')
-            ax1.set_xscale('log')
-            ax1.set_xlabel('seconds')
-            ax1.set_xticks([0.001,1,60,3600,24*3600])
-            ax1.set_xticklabels(['1ms','1s','1min','1h','1day'])    
-            ax1.set_xlim(0.001,max(iat))
-
-            #print 'xp'
-            xp = filter(lambda v : v>=60, x)
+        sqlq = """SELECT ts,lag(ts) OVER (ORDER BY ts) FROM \
+        (SELECT ts FROM dnsreqs WHERE devid = :d_id UNION ALL \
+        SELECT ts FROM httpreqs2 WHERE devid = :d_id AND user_url = 't' UNION ALL \
+        SELECT startts from flows WHERE devid = :d_id) \
+        AS events"""
+    
+        for elem_id in devids:
+            for row in ses.execute(text(sqlq).bindparams(d_id = elem_id)):
+                if (row[1]==None):
+                    continue
+                iat.append((row[0]-row[1]).total_seconds())
+                    #if (row[0]-row[1]).total_seconds() > 60*60*8:
+                    #print 'sleep'
+                    #print row[1]
+                    #print 'wake up'
+                    #print row[0]
             
-            #print xp
-            #print 'y'
-            #print y[-len(xp):]
-            ax2.plot(xp,y[-len(xp):], '-b', lw=2)
-            ax2.set_title('Zoom 1 [values=%d]'%(len(xp)))    
-            ax2.set_ylabel('CDF')
-            ax2.set_xscale('log')
-            ax2.set_xlabel('seconds')
-            ax2.set_xticks([60,600,3600,24*3600])
-            ax2.set_xticklabels(['1min','10min','1h','1day'])        
-            ax2.set_xlim(60,max(iat))
+            if iat and user.username != 'desir':
+                plot_interval_times(iat, user, devs, elem_id)
+
+
+
+def plot_interval_times(iat, user, devs, elem_id):
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+    (x,y) = datautils.aecdf(iat)
+    ax1.plot(x,y, '-b', lw=2)
+    ax1.set_title('Inter-Event Time [user=%s, events=%d]'%(user.username, len(x)))    
+    ax1.set_ylabel('CDF')
+    ax1.set_xscale('log')
+    ax1.set_xlabel('seconds')
+    ax1.set_xticks([0.001,1,60,3600,24*3600])
+    ax1.set_xticklabels(['1ms','1s','1min','1h','1day'])    
+    ax1.set_xlim(0.001,max(iat))
+    
+    xp = filter(lambda v : v>=60, x)            
+    ax2.plot(xp,y[-len(xp):], '-b', lw=2)
+    ax2.set_title('Zoom 1 [values=%d]'%(len(xp)))    
+    ax2.set_ylabel('CDF')
+    ax2.set_xscale('log')
+    ax2.set_xlabel('seconds')
+    ax2.set_xticks([60,600,3600,24*3600])
+    ax2.set_xticklabels(['1min','10min','1h','1day'])        
+    ax2.set_xlim(60,max(iat))
                 
-            xp = filter(lambda v : v>=600, x)
-            ax3.plot(xp,y[-len(xp):], '-b', lw=2)
-            ax3.set_title('Zoom 2 [values=%d]'%(len(xp)))    
-            ax3.set_ylabel('CDF')
-            ax3.set_xscale('log')
-            ax3.set_xlabel('seconds')
-            ax3.set_xticks([600,3600,3*3600,12*2600,24*3600,3*24*3600,7*24*3600])
-            ax3.set_xticklabels(['10min','1h','3h','12h','1d','3d','1w'])        
-            ax3.set_xlim(600,max(iat))
+    xp = filter(lambda v : v>=600, x)
+    ax3.plot(xp,y[-len(xp):], '-b', lw=2)
+    ax3.set_title('Zoom 2 [values=%d]'%(len(xp)))    
+    ax3.set_ylabel('CDF')
+    ax3.set_xscale('log')
+    ax3.set_xlabel('seconds')
+    ax3.set_xticks([600,3600,3*3600,12*2600,24*3600,3*24*3600,7*24*3600])
+    ax3.set_xticklabels(['10min','1h','3h','12h','1d','3d','1w'])        
+    ax3.set_xlim(600,max(iat))
     
-            plt.tight_layout()
-            
-            #fig.subplots_adjust(hspace = .8)
-            print devs[int(elem_id)]
-            fig.savefig('figs_CDF/%s-%s.png' % (user.username, devs[int(elem_id)]))
-            print 'passou'
-            plt.close(fig)
+    plt.tight_layout()
+    fig.savefig('figs_CDF/%s-%s.png' % (user.username, devs[int(elem_id)]))
+    plt.close(fig)
+
+
+if __name__ == "__main__":
+    main()
