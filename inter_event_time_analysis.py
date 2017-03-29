9eb927fd70f7aab76b11c0d3edf43d80609c0cb5
Marina Leao Lucena <mleaoluc@ucn.inria.fr>
2017-03-29 16:15:40 +0200

    solved xp problem

--- inter_event_time_analysis.py
+++ inter_event_time_analysis.py
@@ -51,11 +51,13 @@ def main ():
         for d in user.devices:
             devs[d.id] = d.platform
     
-        sqlq = contains_blacklist(0)
-    
+        #sqlq = contains_blacklist(0)
+        url = '\'%http://c.go-mpulse.net%\''
+        sqlq = get_inter_event_query(url)
+        
         sqlq_flow = """SELECT startts, endts FROM flows WHERE devid = :d_id"""
-
         for elem_id in devids:
+            print elem_id
             iat = []
             traces = []
             for row in ses.execute(text(sqlq).bindparams(d_id = elem_id)):
@@ -63,6 +65,7 @@ def main ():
                     continue
                 iat.append((row[0]-row[1]).total_seconds())
                 traces.append(row[0])
+
                 #if (row[0]-row[1]).total_seconds() > 60*60*2:
                     #print 'sleep'
                     #print row[1]
@@ -75,10 +78,11 @@ def main ():
                 flow_beg.append(row[0])
                 flow_end.append(row[1])
             
-            #if iat and user.username != 'desir':
-            if flow_beg: 
-                #plot_cdf_interval_times(iat, user, devs, elem_id)
-                plot_traces(traces, flow_beg, flow_end, user, devs, elem_id)
+            if iat:
+                plot_cdf_interval_times(iat, user, devs, elem_id, url)
+            
+            #if flow_beg:
+                #plot_traces(traces, flow_beg, flow_end, user, devs, elem_id)
 
 
 def plot_traces(traces, flow_beg, flow_end, user, devs, elem_id): 
@@ -110,7 +114,7 @@ def plot_traces(traces, flow_beg, flow_end, user, devs, elem_id):
     hfmt = dates.DateFormatter('%m-%d')
     ax.yaxis.set_major_formatter(hfmt)
     ax.plot(x,y, '.g')
-    #ax.hlines(y, x_beg, x_end, 'g')
+    ax.hlines(y, x_beg, x_end, 'g')
     
     ax.set_title('Device usage [user=%s, device=%s]'%(user.username, devs[int(elem_id)]))
     ax.set_ylabel('Date')
@@ -132,10 +136,11 @@ def plot_traces(traces, flow_beg, flow_end, user, devs, elem_id):
 
 
 
-def plot_cdf_interval_times(iat, user, devs, elem_id):
+def plot_cdf_interval_times(iat, user, devs, elem_id, url):
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
     (x,y) = datautils.aecdf(iat)
+
     ax1.plot(x,y, '-b', lw=2)
     ax1.set_title('Inter-Event Time [user=%s, events=%d]'%(user.username, len(x)))    
     ax1.set_ylabel('CDF')
@@ -144,29 +149,33 @@ def plot_cdf_interval_times(iat, user, devs, elem_id):
     ax1.set_xticks([0.001,1,60,3600,24*3600])
     ax1.set_xticklabels(['1ms','1s','1min','1h','1day'])    
     ax1.set_xlim(0.001,max(iat))
+
     
     xp = filter(lambda v : v>=60, x)            
-    ax2.plot(xp,y[-len(xp):], '-b', lw=2)
-    ax2.set_title('Zoom 1 [values=%d]'%(len(xp)))    
-    ax2.set_ylabel('CDF')
-    ax2.set_xscale('log')
-    ax2.set_xlabel('seconds')
-    ax2.set_xticks([60,600,3600,24*3600])
-    ax2.set_xticklabels(['1min','10min','1h','1day'])        
-    ax2.set_xlim(60,max(iat))
-                
+    if xp:
+        ax2.plot(xp,y[-len(xp):], '-b', lw=2)
+        ax2.set_title('Zoom 1 [values=%d]'%(len(xp)))    
+        ax2.set_ylabel('CDF')
+        ax2.set_xscale('log')
+        ax2.set_xlabel('seconds')
+        ax2.set_xticks([60,600,3600,24*3600])
+        ax2.set_xticklabels(['1min','10min','1h','1day'])        
+        ax2.set_xlim(60,max(iat))
+
     xp = filter(lambda v : v>=600, x)
-    ax3.plot(xp,y[-len(xp):], '-b', lw=2)
-    ax3.set_title('Zoom 2 [values=%d]'%(len(xp)))    
-    ax3.set_ylabel('CDF')
-    ax3.set_xscale('log')
-    ax3.set_xlabel('seconds')
-    ax3.set_xticks([600,3600,3*3600,12*2600,24*3600,3*24*3600,7*24*3600])
-    ax3.set_xticklabels(['10min','1h','3h','12h','1d','3d','1w'])        
-    ax3.set_xlim(600,max(iat))
+    if xp:
+        print 'entrou'
+        ax3.plot(xp,y[-len(xp):], '-b', lw=2)
+        ax3.set_title('Zoom 2 [values=%d]'%(len(xp)))    
+        ax3.set_ylabel('CDF')
+        ax3.set_xscale('log')
+        ax3.set_xlabel('seconds')
+        ax3.set_xticks([600,3600,3*3600,12*2600,24*3600,3*24*3600,7*24*3600])
+        ax3.set_xticklabels(['10min','1h','3h','12h','1d','3d','1w'])        
+        ax3.set_xlim(600,max(iat))
     
     plt.tight_layout()
-    fig.savefig('figs_CDF/%s-%s.png' % (user.username, devs[int(elem_id)]))
+    fig.savefig('figs_CDF/%s-%s-mpulse.png' % (user.username, devs[int(elem_id)]))
     plt.close(fig)
 
 def contains_blacklist (var):
@@ -191,6 +200,10 @@ def contains_blacklist (var):
     SELECT ts FROM httpreqs2 WHERE devid = :d_id AND matches_urlblacklist = 'f' AND user_url = 't') \
     AS events;"""
 
+def get_inter_event_query(url):
+
+    return ("""SELECT ts, lag(ts) OVER (ORDER BY ts) FROM httpreqs WHERE \
+    devid = :d_id AND req_url LIKE %s ORDER BY ts""")%(url)
     
 
 if __name__ == "__main__":
